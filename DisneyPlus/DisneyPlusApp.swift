63c04e1090733deabcad9065fc488683d79aa806
goutham <gouthamshine@gmail.com>
2022-07-05 12:36:44 +0530

    Initial Commit

---
+++ DisneyPlus/DisneyPlusApp.swift
@@ -0,0 +1,17 @@
+//
+//  DisneyPlusApp.swift
+//  DisneyPlus
+//
+//  Created by Goutham S on 05/07/22.
+//
+
+import SwiftUI
+
+@main
+struct DisneyPlusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
